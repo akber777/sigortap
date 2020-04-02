70efe969bd0edde1c436e44d5b3a5ea4c4e5c534
Akber Akhmedzadeh <akber_akhmedzadeh@mail.ru>
2020-04-02 23:13:03 +0400

    validation

--- src/js/main/required.js
+++ src/js/main/required.js
@@ -5,9 +5,88 @@ $(document).ready(function(){
 
 
 	
-	let submitInputs=$('.search_top input')
+	let submitInputs=document.querySelectorAll('.search_top input')[0];
 
 
+	let myinp=$(".search_top input");
+
+
+	submitInputs.onclick=function(event){
+
+
+		let myinput=document.querySelectorAll('input.required');
+
+
+		for(let i=0;i<myinput.length;i++)
+		{
+
+			let myinpVal=myinput[i].value;
+
+
+			if(myinpVal.trim()=="")
+			{
+
+				let myInputsChange=myinput[i];
+
+
+				myInputsChange.classList.add('invalidInputs')
+				
+
+				event.preventDefault();
+
+			}
+			else{
+
+				let myInputsChange=myinput[i];
+
+				myInputsChange.classList.remove('invalidInputs')
+			}
+
+			
+		}
+
+
+		// end
+
+
+	}
+
+
+
+
+
+
+
+	myinp.click(function(event){
+
+
+		let myform=$(this).parents('form');
+
+		myform.find('select').each(function(){
+
+
+			
+
+
+			if($(this).find('option:selected').val()=="")
+			{
+				$(this).addClass('invalidInputs');
+
+				event.preventDefault();
+
+			}else{
+
+				$(this).removeClass('invalidInputs')
+			}
+
+
+
+
+
+		})
+
+
+	})
 
 
 
