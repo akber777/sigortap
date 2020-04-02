fd21c78be2bcca1efc658f6a79e32a136635d969
Akber Akhmedzadeh <akber_akhmedzadeh@mail.ru>
2020-04-03 01:03:09 +0400

    validation updates

--- src/js/main/required.js
+++ src/js/main/required.js
@@ -5,72 +5,94 @@ $(document).ready(function(){
 
 
 	
-	let submitInputs=document.querySelectorAll('.search_top input')[0];
+	// let submitInputs=document.querySelectorAll('.search_top input')[0];
+
+
+	// submitInputs.onclick=function(event){
+
+
+	// 	let myinput=document.querySelectorAll('input.required');
+
+
+	// 	for(let i=0;i<myinput.length;i++)
+	// 	{
+
+	// 		let myinpVal=myinput[i].value;
+
+
+	// 		if(myinpVal.trim()=="")
+	// 		{
+
+	// 			let myInputsChange=myinput[i];
+
+
+	// 			myInputsChange.classList.add('invalidInputs')
+
+
+	// 			event.preventDefault();
+
+	// 		}
+	// 		else{
+
+	// 			let myInputsChange=myinput[i];
+
+	// 			myInputsChange.classList.remove('invalidInputs')
+	// 		}
+
+
+	// 	}
+
+
+	// 	// end
+
+
+	// }
+
+
+
 
 
 	let myinp=$(".search_top input");
 
 
-	submitInputs.onclick=function(event){
-
-
-		let myinput=document.querySelectorAll('input.required');
-
-
-		for(let i=0;i<myinput.length;i++)
-		{
-
-			let myinpVal=myinput[i].value;
-
-
-			if(myinpVal.trim()=="")
-			{
-
-				let myInputsChange=myinput[i];
-
-
-				myInputsChange.classList.add('invalidInputs')
-				
-
-				event.preventDefault();
-
-			}
-			else{
-
-				let myInputsChange=myinput[i];
-
-				myInputsChange.classList.remove('invalidInputs')
-			}
-
-			
-		}
-
-
-		// end
-
-
-	}
-
-
-
-
-
-
-
 	myinp.click(function(event){
+
+
+		// ---------------end
 
 
 		let myform=$(this).parents('form');
 
-		myform.find('select').each(function(){
+
+		myform.find('input.required').each(function(){
 
 
-			
+			if($(this).val()=="")
+			{
+				$(this).addClass('invalidInputs');
+
+				event.preventDefault();
+			}
+			else{
+				$(this).removeClass('invalidInputs')
+			}
+
+
+		})
+
+		
+
+
+		// 	end ------------------
+
+
+		myform.find('select.required').each(function(){
 
 
 			if($(this).find('option:selected').val()=="")
 			{
 				$(this).addClass('invalidInputs');
+
 
 				event.preventDefault();
 
@@ -78,10 +100,6 @@ $(document).ready(function(){
 
 				$(this).removeClass('invalidInputs')
 			}
-
-
-
-
 
 		})
 
