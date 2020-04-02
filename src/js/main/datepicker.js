1cc4bf979155f62e9aa92d92dfaef6a03b2a5771
Akber Akhmedzadeh <akber_akhmedzadeh@mail.ru>
2020-04-02 15:12:36 +0400

    required

--- src/js/main/datepicker.js
+++ src/js/main/datepicker.js
@@ -26,8 +26,6 @@ $(document).ready(function(){
 		$( "#datepickerEnd" ).datepicker('setStartDate',datestart)
 
 
-
-		console.log(datestart)
 	})
 
 
@@ -163,6 +161,68 @@ for(key of mymap2)
 
 
 // end
+
+
+
+
+
+
+let dateyear = new Date();
+
+let newdatesyear=dateyear.toString().split(" ");
+
+
+let mymapyear=new Map();
+
+mymapyear.set(newdatesyear,'mapsnew');
+
+for(key of mymapyear)
+{
+
+	let parseDate=parseInt(key[0][3] - '65');
+
+	let addString=key[0]
+
+	let index = addString.indexOf(key[0][3]);
+
+	if (index !== -1) {
+
+
+		let myoldkeys=key[0][index] = parseDate;
+
+		let thisMonth=new Date();
+
+		let montIndex='0'+thisMonth.getMonth()
+
+
+		let mydates=new Date(key[0][3],montIndex,key[0][2]);
+
+		
+		$("#ekConstructionYear").datepicker({
+			format: "yyyy",
+			viewMode: "years", 
+			minViewMode: "years",
+		});
+
+
+
+		$( "#ekConstructionYear" ).datepicker('setEndDate',dateyear);
+
+	}
+
+
+	// -----------------
+
+}
+
+
+
+
+
+// end
+
+
+
 
 
 
--- src/js/main/required.js
+++ src/js/main/required.js
@@ -4,19 +4,61 @@ $(document).ready(function(){
 
 
 
+	
+	let submitInputs=$('.search_top input')
 
 
 
 
 
+	$(document).on('submit',submitInputs,function(e){
+
+
+		$('form input[class="required"]').each(function(){
+
+
+			let inpval=$(this).val();
+
+
+			if(inpval.trim()=="")
+			{	
+				$(this).addClass('invalidInputs');
+
+				e.preventDefault();
+			}
+			if(inpval.trim()!="")
+			{
+				$(this).removeClass('invalidInputs');
+			}
+
+
+		})
 
 
 
+		$( '#travel_form select[class="required"] option:selected' ).each(function(){
+
+
+			let selectVal=$(this).val();
+
+
+			if(selectVal.trim()=="")
+			{	
+				$(this).parent('select').addClass('invalidInputs');
+
+				e.preventDefault();
+			}
+			if(selectVal.trim()!="")
+			{
+				$(this).parent('select').removeClass('invalidInputs');
+			}
+
+
+		})
 
 
 
-
-
+	})
 
 
 
